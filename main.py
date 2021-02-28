ef77aad621d243e30ea819e750d4a8e026bb89d7
jzaldivar <jorge.zaldivar@gmail.com>
2021-02-28 13:36:23 -0600

    Delete solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-
+"""
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,23 +13,3 @@ entrada = input()
 
 # Salidas
 print(salida)
-"""
-# Entradas
-anho = eval(input("Introduzca un año: "))
-	
-# Proceso
-if anho % 100 == 0:
-    # Año secular, tiene que ser divisible entre 400
-    if anho % 400 == 0:
-        resultado = "sí es"
-    else:
-        resultado = "no es"
-else:
-    # Año no secular, tiene que ser divisible entre 4
-	if anho % 4 == 0:
-		resultado = "sí es"
-	else:
-		resultado = "no es"
-	
-# Salidas
-print("El año", anho, resultado, "bisiesto.")
