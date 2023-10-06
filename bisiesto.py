bcac9bb9659aa836eb59b28d6bcc633a2b92c401
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-05 20:44:21 -0600

    Remove solution

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,15 @@
 """
-Determinar si un año es bisiesto o no.
+Inserta el encabezado aquí y escribe tu código abajo
 """
 
+# Declaraciones
+CONSTANTE = valor
+
 # Entradas
-anho = int(input("Introduzca el año: "))
+entrada = input()
 
 # Proceso
-if anho % 400 == 0 or anho % 4 == 0 and anho % 100 != 0:
-    bisiesto = "sí"
-else:
-    bisiesto = "no"
+
 
 # Salidas
-print(f"El año {anho} {bisiesto} es bisiesto")
+print(salida)
